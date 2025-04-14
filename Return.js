4bdb87f4164f00a1b1e4a925ddca19ba1c62d873
ARST113 <96774211+ARST113@users.noreply.github.com>
2025-04-14 13:13:20 +0300

    Update Return.js

--- Return.js
+++ Return.js
@@ -1,15 +1,12 @@
 (function () {
     console.log('[Return] Старт выполнения плагина');
 
-    // Проверка готовности Lampa
     function waitForLampaReady() {
         if (window.appready || window.Lampa?.Storage) {
             console.log('[Return] Lampa готова');
 
-            // Устанавливаем флаг
             window.plugin_continue_watch_ready = true;
 
-            // Запуск логики плагина
             setupContinueButton();
         } else {
             console.log('[Return] Ожидаем Lampa...');
@@ -17,45 +14,51 @@
         }
     }
 
-    // Логика добавления кнопки "Продолжить просмотр"
     function setupContinueButton() {
-        console.log('[Return] Запускаем логику кнопки "Продолжить просмотр"');
+        console.log('[Return] Подписка на событие полной загрузки карточки');
 
-        const lastTorrent = Lampa.Storage.get('parser_torrent_view');
-        console.log('[Return] Последний просмотренный торрент:', lastTorrent);
+        Lampa.Listener.follow('full', function (event) {
+            if (event.type !== 'complite') return;
 
-        if (lastTorrent) {
-            Lampa.Listener.follow('full', function (event) {
-                if (event.type === 'complite' && event.data?.movie?.id === lastTorrent.id) {
-                    console.log('[Return] Совпадение ID! Добавляем кнопку "Продолжить просмотр"...');
+            const movie = event.data.movie;
+            const parser = Lampa.Storage.get('parser_torrent_view', []);
+            
+            console.log('[Return] Все записи parser_torrent_view:', parser);
+            console.log('[Return] ID текущего фильма:', movie.id);
 
-                    let button = document.createElement('div');
-                    button.classList.add('selector', 'button--continue');
-                    button.innerHTML = `
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M8 5v14l11-7z"/></svg>
-                        <span>Продолжить просмотр</span>
-                    `;
+            const last = parser.find(e => e.id == movie.id);
+            console.log('[[Return]] Последний просмотренный торрент:', last);
 
-                    button.addEventListener('click', () => {
-                        console.log('[Return] Нажата кнопка "Продолжить просмотр"');
-                        Lampa.Player.play({
-                            title: lastTorrent.title,
-                            url: lastTorrent.url
-                        });
-                    });
+            if (!last) {
+                console.log('[Return] Торрент в хранилище не найден');
+                return;
+            }
 
-                    let buttonsContainer = document.querySelector('.full-start__buttons');
-                    if (buttonsContainer) {
-                        buttonsContainer.prepend(button);
-                        console.log('[Return] Кнопка добавлена на страницу');
-                    } else {
-                        console.log('[Return] Не найден контейнер для кнопок');
-                    }
-                }
+            console.log('[Return] Совпадение найдено! Добавляем кнопку');
+
+            let button = document.createElement('div');
+            button.classList.add('selector', 'button--continue');
+            button.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M8 5v14l11-7z"/></svg>
+                <span>Продолжить просмотр</span>
+            `;
+
+            button.addEventListener('click', () => {
+                console.log('[Return] Нажата кнопка "Продолжить просмотр"');
+                Lampa.Player.play({
+                    title: last.title || movie.title,
+                    url: last.url
+                });
             });
-        } else {
-            console.log('[Return] Торрент в хранилище не найден');
-        }
+
+            let buttonsContainer = document.querySelector('.full-start__buttons');
+            if (buttonsContainer) {
+                buttonsContainer.prepend(button);
+                console.log('[Return] Кнопка добавлена на страницу');
+            } else {
+                console.log('[Return] Не найден контейнер для кнопок');
+            }
+        });
     }
 
     waitForLampaReady();
