3d6f3ee98b1161a79482fea177977f937e265a6b
ARST113 <96774211+ARST113@users.noreply.github.com>
2025-04-18 16:05:48 +0300

    Update Debuger.js

--- Debuger.js
+++ Debuger.js
@@ -1,121 +1,72 @@
-;(function(){
-  // Регистрация плагина в Lampa
-  const plugin = {
-    name: 'MyTorrentPlugin',         // уникальное имя плагина
-    version: '1.0',
-    icon: 'magnet',                  // иконка в списке источников
-    searchOnStart: false,            // отключаем автопоиск при загрузке
+(function(){
+    'use strict';
 
-    // Поиск по ключевому слову
-    search(page, query, callback) {
-      Lampa.Utils.log('MyTorrentPlugin', 'search', query);
-      const url = `https://your.api/search?q=${encodeURIComponent(query)}&page=${page}`;
-      $.get({
-        url,
-        dataType: 'json',
-        timeout: 15000
-      }).done(data => {
-        if(!data || !data.results || !data.results.length){
-          Lampa.Noty.show('Ничего не найдено');
-          return;
-        }
-        // Преобразуем ответ API в единый формат
-        const items = data.results.map(item => ({
-          title: item.title,
-          year:  item.year,
-          torrent: item.magnet,      // magnet-ссылка или URL .torrent
-          infoHash: item.hash,
-          poster: item.poster
-        }));
-        callback(items, data.pages);
-      }).fail(err => {
-        Lampa.Utils.log('MyTorrentPlugin', 'search fail', err);
-        Lampa.Noty.show(`Ошибка поиска: ${err.statusText || err}`);
-      });
-    },
-
-    // Получение деталей (необязательно, можно сразу играть)
-    find(item, callback) {
-      Lampa.Utils.log('MyTorrentPlugin', 'find', item);
-      callback(item);
-    },
-
-    // Основной метод запуска плеера
-    play(item) {
-      Lampa.Utils.log('MyTorrentPlugin', 'play item', item);
-
-      // Базовые проверки
-      if(!item || (!item.torrent && !item.infoHash)){
-        Lampa.Noty.show('Нет ссылки на торрент или magnet');
-        return;
-      }
-
-      // Сохраним хэш в Storage для возможности повторных попыток
-      if(item.infoHash){
-        Lampa.Storage.set('my_plugin_last_hash', item.infoHash);
-      }
-
-      // Выбор URL
-      const torrentUrl = item.torrent || `magnet:?xt=urn:btih:${item.infoHash}`;
-
-      // Запросим сам torrent-файл (если это прямой URL), или сразу генерируем playlist из magnet
-      if(torrentUrl.startsWith('http')) {
-        $.get({
-          url: torrentUrl,
-          timeout: 15000,
-          xhrFields: { responseType: 'arraybuffer' }
-        }).done(data => {
-          Lampa.Utils.log('MyTorrentPlugin', 'torrent downloaded', data);
-
-          if(!data || data.byteLength === 0){
-            Lampa.Noty.show('Файл .torrent пуст или недоступен');
-            return;
-          }
-
-          // Здесь вам понадобится парсер .torrent, например parse-torrent.js
-          // const parsed = parseTorrent(data);
-          // if(!parsed.files || !parsed.files.length){
-          //   Lampa.Noty.show('Не удалось распарсить торрент');
-          //   return;
-          // }
-          //
-          // const playlist = parsed.files.map(file => ({
-          //   title: file.name,
-          //   url: URL.createObjectURL(new Blob([data])),
-          //   size: file.length
-          // }));
-
-          // Для упрощения сделаем одну запись (все данные в одном потоке)
-          const playlist = [{
-            title: item.title + (item.year ? ` (${item.year})` : ''),
-            url: URL.createObjectURL(new Blob([data])),
-            torrent_hash: item.infoHash
-          }];
-
-          // Запускаем плеер
-          Lampa.Player.play({
-            title:       item.title,
-            poster:      item.poster,
-            torrent_hash:item.infoHash,
-            playlist:    playlist
-          });
-        }).fail(err => {
-          Lampa.Utils.log('MyTorrentPlugin', 'torrent download fail', err);
-          Lampa.Noty.show(`Не удалось загрузить торрент: ${err.statusText || err}`);
-        });
-      }
-      else {
-        // magnet-ссылка: сразу пускаем
-        Lampa.Player.play({
-          title:        item.title,
-          poster:       item.poster,
-          url:          torrentUrl,
-          torrent_hash: item.infoHash
-        });
-      }
+    // Добавляем стили для кнопки
+    var style = `
+    .view--continue {
+      background-color: #15bdff;
+      color: #fff;
     }
-  };
+    .view--continue.focus,
+    .view--continue:hover {
+      background-color: #0d8ad1;
+    }
+    `;
+    $('head').append('<style>' + style + '</style>');
 
-  // Регистрируем плагин как источник в Lampa
-  Lampa.Source.add(plugin);
+    /**
+     * Добавить кнопку "Продолжить просмотр" рядом со стандартной кнопкой
+     * @param {jQuery} render  - контейнер c кнопкой запуска (.view--torrent)
+     * @param {Object} card    - объект карточки (movie) с полями url, quality, title и т.д.
+     */
+    function addContinueBtn(render, card) {
+        // Получаем сохранённый прогресс
+        var views = Lampa.Storage.get('file_view', {});
+        var key   = card.torrent_hash || card.id;
+        var v     = views[key];
+
+        if(v && v.percent > 0 && v.percent < 100) {
+            // Формируем кнопку
+            var btn = $(
+                '<div class="full-start__button selector view--continue" ' +
+                'data-subtitle="' + Lampa.Utils.secondsToTime(v.time, true) + '/' + Lampa.Utils.secondsToTime(v.duration, true) + '">Продолжить просмотр</div>'
+            );
+
+            // При клике вызываем плеер с передачей timeline
+            btn.on('hover:enter click', function(){
+                Lampa.Player.play(Object.assign({}, card, {
+                    timeline: {
+                        time:     v.time,
+                        duration: v.duration
+                    }
+                }));
+            });
+
+            // Вставляем кнопку после стандартной
+            render.after(btn);
+        }
+    }
+
+    // Слушаем событие полного просмотра карточки
+    Lampa.Listener.follow('full', function(e) {
+        if(e.type === 'complite') {
+            var root  = e.object.activity.render();
+            var node  = root.find('.view--torrent');
+            var movie = e.data.movie;
+
+            if(node.length && root.find('.view--continue').length === 0) {
+                addContinueBtn(node, movie);
+            }
+        }
+    });
+
+    // Регистрируем плагин в манифесте
+    Lampa.Manifest.plugins = Lampa.Manifest.plugins || [];
+    Lampa.Manifest.plugins.push({
+        type:        'addon',
+        version:     '1.0.0',
+        name:        'Продолжить просмотр',
+        description: 'Добавляет кнопку «Продолжить просмотр» в полную карточку'
+    });
+
 })();
