dbbae336521466a4e818e81c5aad0ce527a1ffaf
ARST113 <96774211+ARST113@users.noreply.github.com>
2026-02-17 23:49:51 +0300

    Update aet.js

--- aet.js
+++ aet.js
@@ -6,8 +6,6 @@
     const TAG = '[EXT-TIMELINE-DEBUG]';
 
     let externalStarted = false;
-    let wasHidden = false;
-    let lastHash = '';
 
     function log(msg) {
         console.log(TAG, msg);
@@ -19,71 +17,43 @@
         log('INIT');
 
         /* ===== external ===== */
-        Lampa.Listener.on('external', function () {
+        Lampa.Listener.follow('external', function () {
             externalStarted = true;
-            log('EVENT: external → externalStarted = true');
-        });
-
-        /* ===== visibility ===== */
-        document.addEventListener('visibilitychange', function () {
-
-            if (document.hidden) {
-                wasHidden = true;
-                log('EVENT: visibility → hidden');
-            }
-            else {
-                log('EVENT: visibility → visible');
-            }
-
+            log('EVENT: external');
         });
 
         /* ===== state:changed ===== */
-        Lampa.Listener.on('state:changed', function (e) {
+        Lampa.Listener.follow('state:changed', function (e) {
 
             log('EVENT: state:changed');
 
             const target = e.target || e.targer || 'undefined';
             const reason = e.reason || 'undefined';
 
-            log(`state data → target:${target} | reason:${reason}`);
+            log(`DATA → target:${target} | reason:${reason}`);
 
             if (!externalStarted) {
-                log('SKIP: externalStarted = false');
+                log('SKIP: externalStarted=false');
                 return;
             }
 
             if (target !== 'timeline') {
-                log('SKIP: target != timeline');
+                log('SKIP: target!=timeline');
                 return;
             }
 
             if (reason !== 'update') {
-                log('SKIP: reason != update');
+                log('SKIP: reason!=update');
                 return;
             }
 
-            if (!e.data) {
-                log('SKIP: no e.data');
-                return;
-            }
-
-            const hash = e.data.hash;
-            const road = e.data.road;
-
+            const road = e.data && e.data.road;
             if (!road) {
                 log('SKIP: no road');
                 return;
             }
 
-            if (hash === lastHash) {
-                log('SKIP: duplicate hash');
-                return;
-            }
-
-            lastHash = hash;
             externalStarted = false;
-
-            log('PASS: timeline update after external');
 
             const timeText = Lampa.Utils.secondsToTime(road.time, true);
             const durationText = Lampa.Utils.secondsToTime(road.duration || 0, true);
