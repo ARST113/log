15df06b8f7c41307179baa530bc49051db54569f
ARST113 <96774211+ARST113@users.noreply.github.com>
2026-07-30 23:08:42 +0300

    Apply verified Project-A VK link

--- ff2026-vk-extra.js
+++ ff2026-vk-extra.js
@@ -6,6 +6,15 @@ window.FF_VK_EXTRA_READY=fetch("vk-manual-profiles-2.json?v=2",{cache:"no-store"
   .then(function(profiles){
     window.FF_VK_PROFILES=window.FF_VK_PROFILES||{};
     if(profiles&&typeof profiles==="object") Object.assign(window.FF_VK_PROFILES,profiles);
+
+    const verifiedUrls={
+      "Проект-А":"https://vk.com/proekta_okulovka"
+    };
+    Object.entries(verifiedUrls).forEach(function(entry){
+      const title=entry[0],url=entry[1];
+      if(window.FF_VK_PROFILES[title]) window.FF_VK_PROFILES[title].url=url;
+    });
+
     window.FF_VK_META=Object.assign({},window.FF_VK_META||{}, {
       matched:Object.keys(window.FF_VK_PROFILES).length,
       total_schedule_musicians:69,
